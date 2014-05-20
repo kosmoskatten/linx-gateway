f174eb8dddb49e2e60da3a98f033eaa334a4e93e
Patrik Sandahl <kosmoskatten.software@gmail.com>
2014-05-20 22:40:20 +0200

    InterfaceRequest.

--- Network/Linx/Gateway/Message.hs
+++ Network/Linx/Gateway/Message.hs
@@ -1,3 +1,134 @@
-module Network.Linx.Gateway.Message
-       (
-       ) where
+{-# LANGUAGE DeriveGeneric #-}
+module Network.Linx.Gateway.Message where
+
+import Control.Applicative ((<$>), (<*>))
+import Data.Binary
+import Data.Binary.Get (runGet)
+import Data.Int
+import GHC.Generics
+import qualified Data.ByteString.Lazy as LBS
+
+-- | Message.
+data Message =
+  Message !Header !ProtocolPayload
+  deriving (Show, Eq)
+
+-- | Message header.
+data Header =
+  Header !PayloadType !Length
+  deriving (Show, Eq, Generic)
+
+-- | Protocol payload.
+data ProtocolPayload =
+    FailedRequest
+  | InterfaceRequest !Version !Flags
+  deriving (Show, Eq)
+
+-- | Payload type discriminator.
+data PayloadType =
+    InterfaceRequestOp
+  | InterfaceReplyOp
+  deriving (Show, Eq)
+           
+-- | Length descriptor.
+newtype Length = Length Int32
+  deriving (Show, Eq, Generic)
+           
+-- | Version descriptor.
+data Version =
+    V100
+  | Version !Int32
+  deriving (Show, Eq)
+           
+-- | Flags descriptor.
+data Flags =
+    BigEndian
+  | LittleEndian
+  | Flags !Int32
+  deriving (Show, Eq)
+
+-- | Payload class. To be implemented by ProtocolPayload.
+class Payload a where
+  header :: a -> Header
+
+instance Payload ProtocolPayload where
+  header FailedRequest       = error "Shall not be called this way"
+  header InterfaceRequest {} = Header InterfaceRequestOp (Length 8)
+
+-- | Generic binary instances.
+instance Binary Header
+instance Binary Length
+
+-- | Binary instance for 'Message'.
+instance Binary Message where
+  get = error "Shall not be called this way"
+  put (Message msgHeader msgPayload) = put msgHeader >> put msgPayload
+
+-- | Binary instance for 'ProtocolPayload'.
+instance Binary ProtocolPayload where
+  get = error "Shall not be called this way"
+  put FailedRequest = error "Shall not be called this way"
+  put (InterfaceRequest version flags) = put version >> put flags
+
+-- | Binary instance for 'PayloadType'.
+instance Binary PayloadType where
+  get = do
+    value <- getInt32
+    return $
+      case value of
+        1 -> InterfaceRequestOp
+        2 -> InterfaceReplyOp
+        _ -> error "Unexpected discriminator code"
+  
+  put InterfaceRequestOp = putInt32 1
+  put InterfaceReplyOp   = putInt32 2
+  
+-- | Binary instance for 'Version'.
+instance Binary Version where
+  get = do
+    value <- getInt32
+    return $
+      case value of
+        100 -> V100
+        _   -> Version value
+  
+  put V100            = putInt32 100
+  put (Version value) = put value
+        
+-- | Binary instance for 'Flags'.
+instance Binary Flags where
+  get = do
+    value <- getInt32
+    return $
+      case value of
+        0          -> BigEndian
+        0x80000000 -> LittleEndian
+        _          -> Flags value
+        
+  put BigEndian     = putInt32 0
+  put LittleEndian  = putInt32 0x80000000
+  put (Flags value) = put value
+
+-- | Make an 'InterfaceRequest' message.
+mkInterfaceRequest :: Message
+mkInterfaceRequest =
+  let payload = InterfaceRequest V100 BigEndian
+  in Message (header payload) payload
+
+decodeProtocolPayload :: PayloadType -> LBS.ByteString -> ProtocolPayload
+decodeProtocolPayload payloadType = runGet go
+  where
+    go :: Get ProtocolPayload
+    go = 
+      case payloadType of
+        InterfaceRequestOp -> decodeInterfaceRequest    
+        _                  -> error "Unsupported payload type"
+        
+decodeInterfaceRequest :: Get ProtocolPayload        
+decodeInterfaceRequest = InterfaceRequest <$> get <*> get
+
+getInt32 :: Get Int32
+getInt32 = get
+  
+putInt32 :: Int32 -> Put
+putInt32 = put
--- test/MessageProperties.hs
+++ test/MessageProperties.hs
@@ -3,10 +3,18 @@ module MessageProperties where
 import Control.Applicative ((<$>), (<*>), pure)
 import Data.Binary
 import Data.Int
-import qualified Data.ByteString.Lazy.Char8 as LBS
+import qualified Data.ByteString.Lazy as LBS
 import Test.QuickCheck hiding (Success)
 import Network.Linx.Gateway.Message
 
+instance Arbitrary Message where
+  arbitrary = return mkInterfaceRequest
 
-prop_message :: Bool
-prop_message = True
+prop_message :: Message -> Bool
+prop_message message@(Message _ msgPayload) =
+  let encodedMessage              = encode message
+      (encHeader, encPayload)     = LBS.splitAt 8 encodedMessage
+      Header msgType (Length len) = decode encHeader
+      msgPayload'                 = decodeProtocolPayload msgType encPayload
+  in msgPayload == msgPayload' 
+     && (fromIntegral len) == (LBS.length encPayload)
